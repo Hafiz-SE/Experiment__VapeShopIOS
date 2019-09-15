ab4943d572a04933292cd788958f87b6d14db54a
Md. Hafizur Rahman <h.rahman1995@outlook.com>
2019-09-15 23:27:26 +0600

    first commit

--- VapeShop/AppDelegate.swift
+++ VapeShop/AppDelegate.swift
@@ -11,11 +11,18 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var window : UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if let controller = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "StartViewController") as? ViewController{
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            self.window?.rootViewController = controller
+            self.window?.makeKeyAndVisible()
+    }
         return true
+        
     }
 
     // MARK: UISceneSession Lifecycle
